a18718c9cd0014393dce35f2c6f804f46b2a8636
Tobykesuma <102707093+Tobykesuma@users.noreply.github.com>
2022-08-24 14:28:56 +0800

    try categories filter

--- src/pages/Scooters/Scooters.jsx
+++ src/pages/Scooters/Scooters.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Scooters.css';
 import {
     Container,
@@ -7,23 +7,30 @@ import {
 } from 'reactstrap';
 import ScooterItem from '../../components/ScooterItem/ScooterItem';
 import scooterData from '../../constant/ScooterData';
-
+import Categories from './Categories';
 
 const Scooters = () => {
+    const [data, setData] = useState(Categories);
+    const filterResult=(ScooterItems)=> {
+        const result=Categories.filter((curData)=>{
+            return curData.category===ScooterItems;
+        });
+        setData(result);
+    }
   return (
     <section>
         <Container>
             <Row>
                 <Col lg='12'>
-                    <div className='d-flex align-items-center gap-3 mb-5'>
+                    <div className='d-flex align-items-center gap-3 mb-5'>  
                         <span className='d-flex align-items-center gap-2'>
                         <i class='bx bx-sort'> Sort By</i>
                         </span>
 
                         <select>
-                            <option>Select</option>
-                            <option value='low'>Low to High</option>
-                            <option value='high'>High to Low</option>
+                            <option onClick={()=>setData(Categories)}>Select</option>
+                            <option value='low' onClick={()=>filterResult('low')}>Low to High</option>
+                            <option value='high' onClick={() => filterResult('high')}>High to Low</option>
                         </select>
                     </div>
                 </Col>
